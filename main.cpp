f6a8880cb52d7f709304c765b17ba15f68ee48ac
immigration2000 <immigration200057@gmail.com>
2026-09-04 17:51:04 +0900

    .
    .

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #pragma comment(lib,"SDL2main")
 
 #include <iostream>
+#include <vector>
 //int main()
 //{
 //	GEngine->Init();
@@ -16,19 +17,24 @@
 //	return 0;
 //}
 
-void DrawCircle(SDL_Renderer* render, int x, int y, int size)
+void DrawCircle(SDL_Renderer* render, SDL_Point point, int size)
 {
 	int CircleX, CircleY = 0;
 
 	for (int i = 0; i < 360; i++)
 	{
-		CircleX = sin(i) * size + x;
-		CircleY = cos(i) * size + y;
+		CircleX = sin(i) * size + point.x;
+		CircleY = cos(i) * size + point.y;
 		SDL_SetRenderDrawColor(render, 255, 255, 255, 0);
 		SDL_RenderDrawPoint(render, CircleX, CircleY);
 	}
 }
-
+enum DrawMode
+{
+	Free,
+	CirCle,
+	Rect
+};
 int SDL_main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -40,12 +46,15 @@ int SDL_main(int argc, char* argv[])
 	SDL_Event Event;
 
 	//그림그리기 변수
-	/*bool Pressed = false;
+	bool Pressed = false;
 	int prev_mouse_x = 0;
-	int prev_mouse_y = 0;*/
+	int prev_mouse_y = 0;
 
 	//원형 찍기
-	
+	std::vector<SDL_Point> list;
+
+	DrawMode Mode = DrawMode::Free;
+
 	while (1)
 	{
 		SDL_PollEvent(&Event);
@@ -59,61 +68,104 @@ int SDL_main(int argc, char* argv[])
 			{
 				break;
 			}
-			
-		}
-		//마우스 기본 셋팅
-		/*if (Event.type == SDL_MOUSEBUTTONUP)
-		{
-
-		}
-		else if (Event.type == SDL_MOUSEBUTTONDOWN)
-		{
-		}
-		else if (Event.type == SDL_MOUSEMOTION && Pressed)
-		{
-		}*/
-		//그림 그리기
-		/*if (Event.type == SDL_MOUSEBUTTONUP)
-		{
-			std::cout << "SDL_BUTTON_LEFT" << std::endl;
-			prev_mouse_x = 0;
-			prev_mouse_y = 0;
-			Pressed = false;
-		}
-		else if (Event.type == SDL_MOUSEBUTTONDOWN)
-		{
-			std::cout << "SDL_MOUSEBUTTONDOWN" << std::endl;
-			Pressed = true;
-			
-		}
-		else if (Event.type == SDL_MOUSEMOTION && Pressed)
-		{
-			int X, Y = 0;
-			SDL_GetMouseState(&X, &Y);
-			if (prev_mouse_x != 0 && prev_mouse_y != 0)
+			if (Event.key.keysym.sym == SDLK_1)
 			{
-				SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 0);
-				SDL_RenderDrawLine(Renderer, prev_mouse_x, prev_mouse_y, X, Y);
+				Mode = DrawMode::Free;
+			}
+			else if (Event.key.keysym.sym == SDLK_2)
+			{
+				Mode = DrawMode::CirCle;
+			}
+			else if (Event.key.keysym.sym == SDLK_3)
+			{
+				Mode = DrawMode::Rect;
 			}
 			
-			std::cout << "Drawing" << std::endl;
-			
-			prev_mouse_x = X;
-			prev_mouse_y = Y;
-		}*/
+		}
+		
 		if (Event.type == SDL_MOUSEBUTTONUP)
 		{
-
+			switch (Mode)
+			{
+			case Free: {
+				prev_mouse_x = 0;
+				prev_mouse_y = 0;
+				Pressed = false;
+				break;
+			}
+			case CirCle:
+				break;
+			case Rect:
+				break;
+			default:
+				break;
+			}
 		}
 		else if (Event.type == SDL_MOUSEBUTTONDOWN)
 		{
-			int X, Y = 0;
-			SDL_GetMouseState(&X, &Y);
-			std::cout << X << Y << std::endl;
-			DrawCircle(Renderer,X,Y,40);
+			switch (Mode)
+			{
+			case Free:
+				Pressed = true;
+				break;
+			case CirCle:
+			{
+				SDL_Point point;
+				point.x = 0;
+				point.y = 0;
+				SDL_GetMouseState(&point.x, &point.y);
+				DrawCircle(Renderer, point, 40);
+				list.push_back(point);
+				break;
+			}
+			case Rect:
+				break;
+			default:
+				break;
+			}
+			
 		}
 		else if (Event.type == SDL_MOUSEMOTION)
 		{
+			switch (Mode)
+			{
+			case Free:
+			{
+				int X, Y = 0;
+				SDL_GetMouseState(&X, &Y);
+				if (prev_mouse_x != 0 && prev_mouse_y != 0)
+				{
+					SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 0);
+					SDL_RenderDrawLine(Renderer, prev_mouse_x, prev_mouse_y, X, Y);
+				}
+
+				std::cout << "Drawing" << std::endl;
+
+				prev_mouse_x = X;
+				prev_mouse_y = Y;
+				break;
+			}
+			case CirCle:
+			{
+				SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 0); // 흰색 배경
+				SDL_RenderClear(Renderer);
+
+				for (int i = 0; i < list.size(); i++)
+				{
+					DrawCircle(Renderer, list[i].x, list[i].y, 40);
+				}
+				SDL_Point point;
+				point.x = 0;
+				point.y = 0;
+				SDL_GetMouseState(&point.x, &point.y);
+				DrawCircle(Renderer, point.x, point.y, 40);
+				break;
+			}
+			case Rect:
+				break;
+			default:
+				break;
+			}
 			
 			
 		}
